8e42eabf20dd3a368570c947e9843a79fdb62454
mamrezn7 <mrz_shirazi@yahoo.com>
2024-09-14 23:21:15 +0330

    adjustment

--- lua/plugins/toggleterm.lua
+++ lua/plugins/toggleterm.lua
@@ -3,11 +3,16 @@ return {
     version = "*",
     config = function()
         require("toggleterm").setup {
-            size = 50,                -- Height for horizontal, width for vertical
             open_mapping = [[<C-\>]], -- Key mapping to toggle terminal
             hide_numbers = true,      -- Hide the number column in terminal buffers
-            shade_terminals = true,
-            direction = "float",      -- Can be 'vertical', 'horizontal', 'tab', or 'float'
+            direction = "float",
+            shade_terminals = true,   -- NOTE: this option takes priority over highlights specified so if you specify Normal highlights you should set this to false
+            shading_factor = 0,       -- the percentage by which to lighten dark terminal background, default: -30
+            shading_ratio = 0,        -- the ratio of shading factor for light/dark terminal background, default: -3
+
+            float_opts = {
+                border = 'curved', },
+
         }
     end,
 }
