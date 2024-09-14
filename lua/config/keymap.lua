6b5f18f52c07a6673222ce7eeff8f420a213e881
Arya Rahmani <AryaSh@edus.local>
2024-09-14 15:58:17 +0330

    my customization

---
+++ lua/config/keymap.lua
@@ -0,0 +1,29 @@
+local opts = { noremap = true }
+vim.api.nvim_set_keymap("i", "<C-s>", "<cmd>w<cr>", opts)
+vim.api.nvim_set_keymap("n", "<C-s>", "<cmd>w<cr>", opts)
+
+--vim.keymap.set({ "i", "n" }, "/", "<cmd>Neotree toggle current reveal_force_cwd<cr>", opts)
+vim.keymap.set("n", "<leader>e", "<cmd>Neotree  toggle <cr>", opts)
+vim.keymap.set({ "i", "n" }, "gd", "<cmd>Neotree float reveal_file=<cfile> reveal_force_cwd<cr>", opts)
+vim.keymap.set({ "i", "n" }, "<leader>b", "<cmd>Neotree toggle show buffers right<cr>", opts)
+vim.keymap.set({ "i", "n" }, "<leader>s", "<cmd>Neotree float git_status<cr>", opts)
+
+vim.keymap.set("n", "<C-l>", "<C-w>l", opts)
+vim.keymap.set("n", "<C-h>", "<C-w>h", opts)
+vim.keymap.set("n", "<C-j>", "<C-w>j", opts)
+vim.keymap.set("n", "<C-k>", "<C-w>k", opts)
+
+vim.keymap.set("n", "<leader>e", "<cmd>NvimTreeToggle<cr>", opts)
+
+vim.keymap.set("n", "<S-l>", "<cmd>BufferLineCycleNext<CR>", opts)
+vim.keymap.set("n", "<S-h>", "<cmd>BufferLineCyclePrev<CR>", opts)
+
+vim.keymap.set("n", "<leader>bq", "<cmd>BufferLinePickClose<CR>", opts)
+
+vim.keymap.set("n", "<leader>|", "<C-w>v", opts)
+vim.keymap.set("n", "<leader>-", "<C-w>s", opts)
+
+vim.keymap.set("n", "<C-Up>", "<cmd>resize +2<CR>", opts)
+vim.keymap.set("n", "<C-Down>", "<cmd>resize -2<CR>", opts)
+vim.keymap.set("n", "<C-Left>", "<cmd>vertical resize -2<CR>", opts)
+vim.keymap.set("n", "<C-Right>", "<cmd>vertical resize +2<CR>", opts)
---
+++ lua/plugins/bufferline.lua
@@ -0,0 +1,22 @@
+return {
+	"akinsho/bufferline.nvim",
+	version = "*",
+	dependencies = "nvim-tree/nvim-web-devicons",
+	config = function()
+		local bufferline = require("bufferline")
+
+		bufferline.setup({
+			options = {
+
+				offsets = {
+					{
+						filetype = "NvimTree",
+						text = "File Explorer",
+						text_align = "left",
+						separator = true,
+					},
+				},
+			},
+		})
+	end,
+}
