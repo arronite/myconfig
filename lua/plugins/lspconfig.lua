838364c9b105d2fbb6676902c9f6601678db60a7
Arya Rahmani <AryaSh@edus.local>
2024-09-17 15:20:15 +0330

    react support

--- lua/plugins/lspconfig.lua
+++ lua/plugins/lspconfig.lua
@@ -10,7 +10,9 @@ return {
             { 'neovim/nvim-lspconfig' },
             { 'hrsh7th/cmp-nvim-lsp' },
             { 'hrsh7th/nvim-cmp' },
-            { "L3MON4D3/LuaSnip" }
+            { "L3MON4D3/LuaSnip" },
+            { 'rafamadriz/friendly-snippets' },
+            { "onsails/lspkind.nvim" }
         },
         config = function()
             local lsp_zero = require('lsp-zero')
@@ -36,6 +38,8 @@ return {
 
             -- CMP setup for autocompletion
             local cmp = require('cmp')
+            local lspkind = require('lspkind')
+            require('luasnip.loaders.from_vscode').lazy_load()
             cmp.setup({
                 snippet = {
                     expand = function(args)
@@ -51,9 +55,25 @@ return {
                     ['<C-e>'] = cmp.mapping.abort(),
                     ['<CR>'] = cmp.mapping.confirm({ select = true }),
                 }),
+                formatting = {
+                    format = lspkind.cmp_format({
+                        mode = 'symbol', -- show only symbol annotations
+                        maxwidth = 50,   -- prevent the popup from showing more than provided characters (e.g 50 will not show more than 50 characters)
+                        -- can also be a function to dynamically calculate max width such as
+                        -- maxwidth = function() return math.floor(0.45 * vim.o.columns) end,
+                        ellipsis_char = '...',    -- when popup menu exceed maxwidth, the truncated part would show ellipsis_char instead (must define maxwidth first)
+                        show_labelDetails = true, -- show labelDetails in menu. Disabled by default
+
+                        -- The function below will be called before any actual modifications from lspkind
+                        -- so that you can provide more controls on popup customization. (See [#30](https://github.com/onsails/lspkind-nvim/pull/30))
+                        before = function(entry, vim_item)
+                            return vim_item
+                        end
+                    })
+                },
                 sources = cmp.config.sources({
-                    { name = 'nvim_lsp' },
                     { name = 'luasnip' }, -- For luasnip users
+                    { name = 'nvim_lsp' },
                 }, {
                     { name = 'buffer' },
                 })
@@ -91,6 +111,7 @@ return {
             cmp.event:on(
                 'confirm_done',
                 cmp_autopairs.on_confirm_done()
+
             )
         end,
     }
