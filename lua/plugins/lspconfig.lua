0ce6adcb6cf87cba19b19d635f9113b419c87b77
arronite <ary45hira2i@gmail.com>
2025-02-27 15:21:01 +0330

    add cmp

--- lua/plugins/lspconfig.lua
+++ lua/plugins/lspconfig.lua
@@ -44,57 +44,65 @@ return {
 			})
 		end,
 	},
+
 	{
 		"hrsh7th/nvim-cmp",
 		dependencies = {
-			"hrsh7th/cmp-buffer", -- Completion from buffer words
-			"hrsh7th/cmp-path", -- Completion for file paths
-			"hrsh7th/cmp-nvim-lsp", -- LSP completions
-			"hrsh7th/cmp-cmdline", -- Command-line completions
-			"saadparwaiz1/cmp_luasnip", -- Snippet completions
+			"hrsh7th/cmp-nvim-lsp",
+			"hrsh7th/cmp-buffer",
+			"hrsh7th/cmp-path",
+			"hrsh7th/cmp-cmdline",
 			"L3MON4D3/LuaSnip",
-			"brenoprata10/nvim-highlight-colors",
-			"onsails/lspkind.nvim", -- Add this
+			"rafamadriz/friendly-snippets",
+			"saadparwaiz1/cmp_luasnip", -- Required for LuaSnip completion
+			"onsails/lspkind.nvim", -- For better icons
 		},
 		config = function()
 			local cmp = require("cmp")
 			local luasnip = require("luasnip")
-			local lspkind = require("lspkind") -- Require lspkind
+			local lspkind = require("lspkind")
 
 			require("luasnip.loaders.from_vscode").lazy_load()
 
 			cmp.setup({
-				sources = {
+				sources = cmp.config.sources({
 					{ name = "nvim_lsp" },
+					{ name = "luasnip" },
 					{ name = "buffer" },
 					{ name = "path" },
-					{ name = "luasnip" },
-				},
+				}),
 				snippet = {
 					expand = function(args)
 						luasnip.lsp_expand(args.body)
 					end,
 				},
 				mapping = cmp.mapping.preset.insert({
-					["<CR>"] = cmp.mapping.confirm({ select = false }),
 					["<Tab>"] = cmp.mapping(function(fallback)
-						local col = vim.fn.col(".") - 1
-
 						if cmp.visible() then
-							cmp.select_next_item({ behavior = "select" })
-						elseif col == 0 or vim.fn.getline("."):sub(col, col):match("%s") then
-							fallback()
+							cmp.select_next_item()
+						elseif luasnip.expand_or_jumpable() then
+							luasnip.expand_or_jump()
 						else
-							cmp.complete()
+							fallback()
 						end
 					end, { "i", "s" }),
+					-- ["<>"] = cmp.mapping(function(fallback)
+					-- 	if cmp.visible() then
+					-- 		cmp.select_prev_item()
+					-- 	elseif luasnip.jumpable(-1) then
+					-- 		luasnip.jump(-1)
+					-- 	else
+					-- 		fallback()
+					-- 	end
+					-- end, { "i", "s" }),
+					["<CR>"] = cmp.mapping.confirm({ select = true }),
 					["<S-Tab>"] = cmp.mapping.complete(),
 				}),
 				formatting = {
 					format = lspkind.cmp_format({
-						mode = "symbol", -- Use icons
-						maxwidth = 50,
-						ellipsis_char = "...",
+						mode = "symbol", -- Show both symbols and text
+						maxwidth = 50, -- Limit max width
+						ellipsis_char = "...", -- Show ellipsis if truncated
 					}),
 				},
 			})
